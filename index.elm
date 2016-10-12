9d3e10aa95c312612e9a80de7d70fa9ae41a700b
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2016-10-11 22:14:57 -0500

    Draw the whole board & update cells on click

--- index.elm
+++ index.elm
@@ -1,8 +1,9 @@
 import Html.App as App
-import Html exposing (..)
+import Html exposing (Html, div, text)
 import Html.Events exposing (onClick)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Svg exposing (svg, circle)
+import Svg.Attributes exposing (width, height, viewBox, fill, stroke, cx, cy, r)
+import Array exposing (..)
 
 main =
   App.beginnerProgram
@@ -11,48 +12,69 @@ main =
     , update = update
     }
 
+
 -- MODEL
 
 type Point = Blank | White | Black
 type Player = WhitePlayer | BlackPlayer
 type alias Model =
-  { board : List (Point,Int)
+  { board : Array (Array (Int, Int, Point))
   , turn : Player
   }
 
-size = 11
+size = 7
+row i = initialize size (\j -> (i, j, Blank))
+board = initialize size (\i -> row i)
+model : Model
+model = {board = board, turn = WhitePlayer}
+
+
+-- UPDATE
+
+type Msg = Update Int Int
+
+update : Msg -> Model -> Model
+update (Update x y) model =
+  let
+    clr = if model.turn == WhitePlayer then White else Black
+    nextTurn = if model.turn == WhitePlayer then BlackPlayer else WhitePlayer
+  in
+    { board = set2 x y (x,y,clr) model.board
+    , turn = nextTurn
+    }
+
+set2 x y val board =
+  case (get x board) of
+    Nothing  -> board
+    Just row -> set x (set y val row) board
+
+
+-- VIEW
+
 radius = 20
 space = radius*3
 boardwidth = (size+1) * space
 lengthstr = toString boardwidth
-states = List.repeat size Blank
-positions = List.scanl (\_ acc->acc+space) space (List.repeat (size-1) 0)
-model : Model
-model = {board = List.map2 (,) states positions, turn = WhitePlayer}
-
--- UPDATE
-
-type Msg = Update Int
-
-update : Msg -> Model -> Model
-update msg model = model
-
--- VIEW
+rr = toString radius
 
 view : Model -> Html Msg
 view model =
-  div [] [ div [] [Html.text ("Whose turn?¿: " ++ (toString model.turn))]
-         , board model.board
+  div [] [ div [] [text ("Whose turn?¿: " ++ (toString model.turn))]
+         , boardSvgArray model.board
          ]
 
-board brd = Svg.svg
-       [ width lengthstr, height lengthstr, viewBox ("0 0 " ++ lengthstr ++ " " ++ lengthstr) ]
-       (List.map cc brd)
+boardSvgArray brd =
+  Svg.svg
+    [ width lengthstr, height lengthstr, viewBox ("0 0 " ++ lengthstr ++ " " ++ lengthstr) ]
+    (List.concat (toList (map f brd)))
 
-cc (stt,pos) =
+f row = toList (map g row)
+g (x,y,stt) =
   let 
+    xx = toString (space * (x+1))
+    yy = toString (space * (y+1))
     clr = case stt of
-    Blank -> "gray"
-    White -> "yellow"
-    Black -> "black"
-  in circle [ onClick (Update pos), fill clr, stroke "black", cx (toString pos), cy "60", r (toString radius) ] []
+      Blank -> "gray"
+      White -> "yellow"
+      Black -> "black"
+  in circle [ onClick (Update x y), fill clr, stroke "black", cx xx, cy yy, r rr ] []
